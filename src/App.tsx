90f58809fd100f1d17273e47ea66f3c0842bdb78
cocoder16 <cocoder16@gmail.com>
2022-07-03 16:50:32 +0900

    [Add] eslint, prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useQuery, useMutation, useQueryClient, QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtoolsPanel } from "react-query/devtools";
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div>Hi</div>
+      <ReactQueryDevtoolsPanel
+        setIsOpen={function (isOpen: boolean): void {
+          throw new Error("Function not implemented.");
+        }}
+        handleDragStart={function (e: React.MouseEvent<HTMLDivElement, MouseEvent>): void {
+          throw new Error("Function not implemented.");
+        }}
+      />
+    </QueryClientProvider>
   );
 }
 
